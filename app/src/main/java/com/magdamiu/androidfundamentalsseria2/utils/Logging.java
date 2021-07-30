8f442e5ae60f008161b76064dc0b4c3579a127d2
Magda Miu <badita.magda@gmail.com>
2021-07-30 19:56:14 +0300

    Update Logging.java

--- app/src/main/java/com/magdamiu/androidfundamentalsseria2/utils/Logging.java
+++ app/src/main/java/com/magdamiu/androidfundamentalsseria2/utils/Logging.java
@@ -1,7 +1,5 @@
 import android.util.Log;
 
-import com.magdamiu.androidfundamentalsseria2.BuildConfig;
-
 // class used in order to log messages in the Logcat window, only in Debug mode
 public class Logging {
   public static void show(Object obj, String message) {
